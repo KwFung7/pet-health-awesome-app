0790810b51a9ab4c2baedb4cf291eb780e47561a
Felix Kwan <kwfxng@gmail.com>
2024-05-16 16:45:42 +0800

    test i18n and styled component

--- src/app/container/home/index.jsx
+++ src/app/container/home/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { ExampleText } from './style';
 import Config from 'react-native-config';
 
 const Home = props => {
+  const { t } = useTranslation();
+
   return (
     <SafeAreaView>
-      <Text>Home Screen Testing</Text>
-      <Text>{Config.TEST_REACT_CONFIG}</Text>
+      <ExampleText>{t('app_name')} - Home Screen Testing</ExampleText>
+      <ExampleText>{Config.TEST_REACT_CONFIG}</ExampleText>
     </SafeAreaView>
   );
 };
